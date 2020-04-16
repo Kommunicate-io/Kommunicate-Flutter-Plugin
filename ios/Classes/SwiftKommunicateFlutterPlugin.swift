753b31b50d51397647573f123b3a5f3fcbf94e2f
Ashish <reytum@live.com>
2020-04-16 18:41:56 +0530

    send error in error callback

--- ios/Classes/SwiftKommunicateFlutterPlugin.swift
+++ ios/Classes/SwiftKommunicateFlutterPlugin.swift
@@ -126,7 +126,10 @@ public class SwiftKommunicateFlutterPlugin: NSObject, FlutterPlugin, KMPreChatFo
             if(Kommunicate.isLoggedIn) {
                 let userClientService = ALUserClientService()
                 userClientService.updateUserDisplayName(kmUser["displayName"] as? String, andUserImageLink: kmUser["imageLink"] as? String, userStatus: kmUser["status"] as? String, metadata: kmUser["metadata"] as? NSMutableDictionary) { (_, error) in
-                    guard error == nil else { return }
+                    guard error == nil else {
+                        self.sendErrorResultWithCallback(result: result, message: error.localizedDescription)
+                        return
+                    }
                     self.sendSuccessResultWithCallback(result: result, message: "Success")
                 }
             } else {
