1dfbdb0d04725bced6d02c359e25909928efea1c
reytum <Neverfight@619>
2021-03-09 17:37:17 +0530

    remove appId from config file

--- example/lib/AppConfig.dart
+++ example/lib/AppConfig.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const APP_ID = '22823b4a764f9944ad7913ddb3e43cae1';
+  static const APP_ID = '<Your-App-Id>';
 
   static bool isValidEmail(String emailId) {
     return RegExp(
